0d8ad1eb97e3d6506e00d7ffe2319be00ba2f3fc
Luis Lobo <luis.lobo9b@gmail.com>
2022-11-21 03:46:44 -0300

    small improvements

--- viewmodel.js
+++ viewmodel.js
@@ -1,18 +1,20 @@
 var ViewModel = (function(){
 
 	var viewModel = function(model){
-		this.bind= function(view_element, model_element){
+		this.bind = function(view_element, model_element){
+			console.log('view_element', view_element);
+			console.log('model_element', model_element);
 			view_element.value = model.getCurrentValue(model_element);
 			model.subscribe(function(attribute_name, newValue){
 				document.getElementsByName(attribute_name).forEach(function(elem){
 					elem.value = newValue;
 				});
 			});
-			view_element.addEventListener('input', function(){
+			view_element.addEventListener("input", function(){
 				model.setCurrentValue(view_element.name, view_element.value);
 			});
 		}
-	}
+	};
 
 	return viewModel;
 })();
